07d12a1a1e564393c453068f16bd4db0a3187779
Kevin Beier <kevinb@codingtemple.com>
2022-05-03 20:44:57 -0400

    halfway day2

--- app.py
+++ app.py
@@ -1,18 +1,53 @@
+
 from flask import Flask, render_template, request
 import requests
+import os
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class Config():
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    REGISTERED_USERS={
+        'kevinb@codingtemple.com':{"name":"Kevin","password":"abc123"},
+        'shohat@codingtemple.com':{"name":"Shoha","password":"Colt45"},
+        'joelc@codingtemple.com':{"name":"Joel", "password":"MorphinTime"}
+    }
+
+class LoginForm(FlaskForm):
+    email = StringField('Email Address', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route('/', methods = ['GET'])
 def index():
     return render_template('index.html.j2')
-
 
 @app.route('/students', methods = ['GET'])
 def students():
     my_students = ['Scarlett', 'Aydee', 'Patrick', 'Armani', 'Jalen']
                                                 # var_name in Jinja = var name in python
     return render_template('students.html.j2', students = my_students)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if request.method=='POST' and form.validate_on_submit():
+        # Do Login stuff
+        email = form.email.data.lower()
+        password = form.password.data
+        if email in app.config.get("REGISTERED_USERS") and password == app.config.get("REGISTERED_USERS").get(email).get('password'):
+            #Login success!!!
+            return f"Login Succes Welcome {app.config.get('REGISTERED_USERS').get(email).get('name')}"
+        error_string = "Incorrect Email/Password Combo"
+        return render_template("login.html.j2", error=error_string, form=form)
+
+    return render_template("login.html.j2", form=form)
 
 @app.route('/ergast', methods=['GET','POST'])
 def ergast():
@@ -46,6 +81,4 @@ def ergast():
             new_data.append(racer_dict)
         
         return render_template('ergast.html.j2', racers=new_data)
-
-
     return render_template('ergast.html.j2')
